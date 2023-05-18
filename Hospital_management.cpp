3a496930babdf5c8bc4ecaa45e0ae7799539a2ae
rs5862 <133974241+rs5862@users.noreply.github.com>
2023-05-18 18:27:32 +0530

    Create Hospital_management.cpp

---
+++ Hospital_management.cpp
@@ -0,0 +1,129 @@
+/**
+* Project Untitled
+*/
+#include "Permission Class.h"
+/**
+* Permission Class implementation
+*/
+void Permission Class::addPermission() {
+}
+void Permission Class::editPermission() {
+}
+void Permission Class::deletePermission() {
+}
+void Permission Class::searchPermission() {
+}
+
+/**
+* Project Untitled
+*/
+#include "Medicine Class.h"
+/**
+* Medicine Class implementation
+*/
+void Medicine Class::addMedicines() {
+}
+void Medicine Class::editMedicines() {
+}
+void Medicine Class::deleteMedicines() {
+}
+void Medicine Class::searchMedicines() {
+}
+
+/**
+* Project Untitled
+*/
+#include "Appointments Classs.h"
+/**
+* Appointments Classs implementation
+*/
+void Appointments Classs::addAppointments() {
+}
+void Appointments Classs::editAppointments() {
+}
+void Appointments Classs::deleteAppointments() {
+}
+void Appointments Classs::searchAppointments() {
+}
+
+/**
+* Project Untitled
+*/
+#include "Doctor Class.h"
+/**
+* Doctor Class implementation
+*/
+void Doctor Class::addDoctors() {
+}
+void Doctor Class::editDoctors() {
+}
+void Doctor Class::deleteDoctors() {
+}
+void Doctor Class::searchDoctors() {
+}
+
+/**
+* Project Untitled
+*/
+#include "Hospital Class.h"
+/**
+* Hospital Class implementation
+*/
+void Hospital Class::addHospitals() {
+}
+void Hospital Class::editHospitals() {
+}
+void Hospital Class::deleteHospitals() {
+}
+void Hospital Class::searchHospitals() {
+}
+
+/**
+* Project Untitled
+*/
+#include "Role Class.h"
+/**
+* Role Class implementation
+*/
+void Role Class::addrole() {
+}
+void Role Class::editrole() {
+}
+void Role Class::deleterole() {
+}
+void Role Class::searchrole() {
+}
+void Role Class::assingrole() {
+}
+
+/**
+* Project Untitled
+*/
+#include "User Class.h"
+/**
+* User Class implementation
+*/
+void User Class::addUser() {
+}
+void User Class::editUser() {
+}
+void User Class::deleteUser() {
+}
+void User Class::searchUser() {
+}
+
+/**
+* Project Untitled
+*/
+#include "Patient Class.h"
+/**
+* Patient Class implementation
+*/
+void Patient Class::addPatient() {
+}
+void Patient Class::editPatient() {
+}
+void Patient Class::deletePatient() {
+}
+void Patient Class::searchPatient() {
+}
